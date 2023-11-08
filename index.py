243f281cda53fa6b75405c909f413d70109bec7e
Emilio Lara <emiliolarap@gmail.com>
2023-11-08 15:15:28 -0600

    XD

--- index.py
+++ index.py
@@ -61,6 +61,8 @@ hsb.pack(side="bottom", fill="x")
 frame = Frame(canvas, background="black")
 frame_id = canvas.create_window((4, 4), window=frame, anchor="nw")
 
+
+
 canvas.bind("<Configure>", on_frame_configure)
 
 # Columna de botones a la izquierda
@@ -132,11 +134,10 @@ def attack1():
     sub = Label(messageWindow, text="Listado de directorio", font="Helvetica 20", bg="black", fg="white")
     sub.pack(side=TOP)
 
-
     #Descomentar para agregar boton si no pasa ssh
-    """ #Regresar a menu principal
+    #Regresar a menu principal
     btn_back= Button(messageWindow, text="Regresar", font=12, height=4, width=30,bg="black", fg="white", command=lambda: main_screen.tkraise()).pack()
-    btn_back.grid(row=0, column=1,padx=10, pady=10, sticky="nsew") """
+    btn_back.grid(row=0, column=1,padx=10, pady=10, sticky="nsew")
 
 
     #Conexion ssh
